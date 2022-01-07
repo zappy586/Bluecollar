5cbc8681aa0e5ce345e7c8afbdfd5acc093f9de2
zappy586 <89218647+zappy586@users.noreply.github.com>
2022-01-08 00:27:31 +0530

    dfhsjfsdj

---
+++ app/src/main/java/com/example/bluecollar/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.bluecollar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
