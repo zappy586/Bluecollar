78e7159aaf0deff8172de689f17d332cf13d2734
Nihal3000 <nihal220403@gmail.com>
2022-01-08 14:56:33 +0530

    Maid work Activity4

--- app/src/main/java/com/example/bluecollar/MainActivity4.java
+++ app/src/main/java/com/example/bluecollar/MainActivity4.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Maid extends AppCompatActivity {
+public class MainActivity4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/bluecollar/WareHouse.java
@@ -0,0 +1,14 @@
+package com.example.bluecollar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WareHouse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ware_house);
+    }
+}
