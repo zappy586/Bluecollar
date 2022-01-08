0c40f58623386978913b8e9ad4d41bbb264f4017
zeeshan <mohdzeesh2002@gmail.com>
2022-01-08 12:07:54 +0530

    intent: Worker - Choose profession

--- app/src/main/java/com/example/bluecollar/MainActivity2.java
+++ app/src/main/java/com/example/bluecollar/MainActivity2.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button button_worker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        button_worker = findViewById(R.id.worker);
+        button_worker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this , MainActivity3.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/bluecollar/MainActivity3.java
+++ app/src/main/java/com/example/bluecollar/MainActivity3.java
@@ -10,5 +10,6 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
     }
 }
