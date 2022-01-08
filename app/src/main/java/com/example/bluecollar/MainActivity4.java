423cbf560fbbea011e63a7ba269a98f4e7070223
zeeshan <mohdzeesh2002@gmail.com>
2022-01-08 20:03:21 +0530

    1

--- app/src/main/java/com/example/bluecollar/MainActivity4.java
+++ app/src/main/java/com/example/bluecollar/MainActivity4.java
@@ -8,20 +8,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity4 extends AppCompatActivity {
 TextView N1;
-EditText N2,N3;
+EditText N2,N3,N4;
 Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-        N2=findViewById(R.id.editTextNumber);
+        N2=findViewById(R.id.work_hours);
         N1=findViewById(R.id.textView8);
         N3=findViewById(R.id.salary_input);
+        N4=findViewById(R.id.work_hours);
         b1=findViewById(R.id.button5);
         b2=findViewById(R.id.button);
         b1.setOnClickListener(new View.OnClickListener() {
@@ -39,17 +39,20 @@ Button b1,b2;
             public void onClick(View view) {
                 String s = N2.getText().toString();
                 String s2 = N3.getText().toString();
+                String s3 = N4.getText().toString();
                 int time = Integer.parseInt (s);
                 int sal = Integer.parseInt(s2);
-                    int Total = sal * time;
-                    N1.setText("the total cost is: "+ Total );
+                int hour = Integer.parseInt(s3);
+                int Total = sal * time;
+                N1.setText("the total cost is: "+ Total );
 
 
-                if(sal>200 || sal<100){
-                    N1.setText("Please enter within limit" );
+                if(sal>300 || sal<200 || hour>7 || hour<0){
+                    N1.setText("Please enter within limit");
+
                 }
             }
         });
-//9
+
     }
 }
