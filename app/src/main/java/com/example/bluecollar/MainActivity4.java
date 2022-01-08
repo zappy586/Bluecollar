1d57edfc82a58775903064f848fdafcab68d4ab0
Nihal3000 <nihal220403@gmail.com>
2022-01-08 12:29:11 +0530

    please select page

--- app/src/main/java/com/example/bluecollar/MainActivity4.java
+++ app/src/main/java/com/example/bluecollar/MainActivity4.java
@@ -1,18 +1,14 @@
 package com.example.bluecollar;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 
-public class MainActivity2 extends AppCompatActivity {
-
+public class MainActivity4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
+        setContentView(R.layout.activity_main4);
     }
 }
