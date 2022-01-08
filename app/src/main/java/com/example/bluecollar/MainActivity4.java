a4fde69e8676058f67e307ac7601a1957ce59499
zeeshan <mohdzeesh2002@gmail.com>
2022-01-08 19:08:22 +0530

    1

--- app/src/main/java/com/example/bluecollar/MainActivity4.java
+++ app/src/main/java/com/example/bluecollar/MainActivity4.java
@@ -2,6 +2,7 @@ package com.example.bluecollar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +13,7 @@ import android.widget.Toast;
 public class MainActivity4 extends AppCompatActivity {
 TextView N1;
 EditText N2,N3;
-Button b;
+Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +22,19 @@ Button b;
         N2=findViewById(R.id.editTextNumber);
         N1=findViewById(R.id.textView8);
         N3=findViewById(R.id.salary_input);
-        b=findViewById(R.id.button5);
-        b.setOnClickListener(new View.OnClickListener() {
+        b1=findViewById(R.id.button5);
+        b2=findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            Intent intent = new Intent(MainActivity4.this, Final.class);
+            startActivity(intent);
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String s = N2.getText().toString();
@@ -32,12 +44,11 @@ Button b;
                 if(sal>300 || sal<200) {
                     int Total = sal * time;
                     N1.setText("the total cost is: "+ Total );
+
                 }
                 else{
                     Toast.makeText(MainActivity4.this, "Enter Within the limit", Toast.LENGTH_SHORT).show();
                 }
-
-
             }
         });
 
